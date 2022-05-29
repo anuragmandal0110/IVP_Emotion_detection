db796ec75286d138709ab2f8ce2e9a7e8d6c8ec9
Anurag Mandal <60498114+anuragmandal0110@users.noreply.github.com>
2022-05-28 21:13:23 -0700

    Create pose_classifier.py

---
+++ pose_classifier.py
@@ -0,0 +1,34 @@
+import torch
+
+class PoseClassification(torch.nn.Module):
+
+    def __init__(self):
+        super(PoseClassification, self).__init__()
+        self.linear1 = torch.nn.Linear(1000, 500)
+        self.activation = torch.nn.ReLU()
+        self.linear2 = torch.nn.Linear(500, 500)
+        self.activation = torch.nn.ReLU()
+        self.linear3 = torch.nn.Linear(500, 100)
+        self.output = torch.nn.Linear(100, 2)
+        self.softmax = torch.nn.Softmax()
+
+    def forward(self, x):
+        x = self.linear1(x)
+        x = self.activation(x)
+        x = self.linear2(x)
+        x = self.activation(x)
+        x = self.linear3(x)
+        x = self.activation(x)
+        x = self.linear3(x)
+        x = self.activation(x)
+        x = self.linear3(x)
+        x = self.activation(x)
+        x = self.linear3(x)
+        x = self.activation(x)
+        x = self.linear3(x)
+        x = self.activation(x)
+        x = self.output(x)
+        x = self.softmax(x)
+        return x
+
+
